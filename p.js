847f62888a2d74c464a160d73606d56e627660eb
zhuzeyu <zhuzeyu00@etest.cn>
2023-04-04 13:46:21 +0800

    first commit

---
+++ p.js
@@ -0,0 +1,9 @@
+export default function Viewer(options) {
+    BaseViewer.call(this, options);
+  }
+
+  Viewer.prototype._modules = [
+  ];
+  
+  // default moddle extensions the viewer is composed of
+  Viewer.prototype._moddleExtensions = {};
