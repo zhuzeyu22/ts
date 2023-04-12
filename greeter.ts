542e166890607fe1f4731143d235a5ecc184b846
zhuzeyu <zhuzeyu00@etest.cn>
2023-04-12 16:44:01 +0800

    update test

--- greeter.ts
+++ greeter.ts
@@ -1,23 +1,35 @@
+const kfr = require('bindings')('kfr.node');
+
 class Student {
-    fullName: string;
-    constructor(
-      public firstName: string,
-      public middleInitial: string,
-      public lastName: string
-    ) {
-      this.fullName = firstName + " " + middleInitial + " " + lastName;
-    }
+  fullName: string;
+  constructor(
+    public firstName: string,
+    public middleInitial: string,
+    public lastName: string
+  ) {
+    this.fullName = firstName + " " + middleInitial + " " + lastName;
   }
-   
-  interface Person {
-    firstName: string;
-    lastName: string;
-  }
-   
-  function greeter(person: Person) {
-    return "Hello, " + person.firstName + " " + person.lastName;
-  }
-   
-  let user = new Student("Jane", "M.", "User");
-   
-  document.body.textContent = greeter(user);
+}
+
+interface Person {
+  firstName: string;
+  lastName: string;
+}
+
+function greeter(person: Person) {
+  return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+function kill(person: Person) {
+  person.firstName = 'qweqwe'
+  return
+}
+
+let user = new Student("Jane", "M.", "User");
+
+// document.body.textContent = greeter(user);
+
+kill(user);
+
+console.log(user)
+console.log(kfr)
--- js/greeter.js
+++ js/greeter.js
@@ -1,13 +1,12 @@
-var kfr = require('bindings')('kfr.node');
-var Student = /** @class */ (function () {
-    function Student(firstName, middleInitial, lastName) {
+"use strict";
+class Student {
+    constructor(firstName, middleInitial, lastName) {
         this.firstName = firstName;
         this.middleInitial = middleInitial;
         this.lastName = lastName;
         this.fullName = firstName + " " + middleInitial + " " + lastName;
     }
-    return Student;
-}());
+}
 function greeter(person) {
     return "Hello, " + person.firstName + " " + person.lastName;
 }
@@ -15,8 +14,8 @@ function kill(person) {
     person.firstName = 'qweqwe';
     return;
 }
-var user = new Student("Jane", "M.", "User");
+let user = new Student("Jane", "M.", "User");
 // document.body.textContent = greeter(user);
 kill(user);
 console.log(user);
-console.log(kfr);
+//# sourceMappingURL=greeter.js.map
