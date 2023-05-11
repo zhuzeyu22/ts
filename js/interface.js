10d60176be29da074388d76b588d24a62613263b
zhuzeyu <zhuzeyu00@etest.cn>
2023-05-11 11:25:30 +0800

    update

--- js/interface.js
+++ js/interface.js
@@ -1,2 +1,45 @@
-export {};
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.C = exports.IB = exports.a = void 0;
+console.log('IB.type');
+function a(serviceId) {
+    const id = function (a, b, c) {
+        if (arguments.length !== 3) {
+            throw new Error('@IServiceName-decorator can only be used to decorate a parameter');
+        }
+    };
+    id.toString = () => serviceId || '';
+    return id;
+}
+exports.a = a;
+exports.IB = a('IB');
+let C = class C {
+    constructor(ib) {
+        this.ib = ib;
+        console.log('ib', ib);
+    }
+};
+C = __decorate([
+    __param(0, exports.IB)
+], C);
+exports.C = C;
+class D {
+    constructor(c) {
+        this.c = c;
+    }
+}
+const d = new D(C);
+console.log('IB.type');
+console.log(d);
+console.log(new d.c());
+console.log(exports.IB.type);
 //# sourceMappingURL=interface.js.map
--- src/interface.ts
+++ src/interface.ts
@@ -1,6 +1,45 @@
 
-export interface ServiceIdentifier<T> {
+
+console.log('IB.type');
+export interface A<T> {
     (...args: any[]): void;
     type: T;
 }
 
+export function a<T>(serviceId?: string): A<T> {
+    const id = <any>function (a: any, b: any, c: any) {
+        if (arguments.length !== 3) {
+            throw new Error('@IServiceName-decorator can only be used to decorate a parameter');
+        }
+    };
+
+    id.toString = () => serviceId || '';
+    return id;
+}
+
+export const IB = a<IB>('IB');
+// export const IB = createDecorator<IB>('IB');
+
+export interface IB {
+    hehe: () => {};
+}
+
+export class C {
+    constructor(
+        @IB
+        readonly ib: IB
+    ) {
+        console.log('ib', ib);
+    }
+}
+
+class D {
+    constructor(public c: any) {
+    }
+}
+
+const d = new D(C);
+// console.log('IB.type');
+console.log(d);
+console.log(new d.c());
+console.log(IB);
