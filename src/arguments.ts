7ffe6773d3a49891615cd7cc95a7e8367712cea4
zhuzeyu <zhuzeyu00@etest.cn>
2023-06-02 10:06:48 +0800

    update

---
+++ src/arguments.ts
@@ -0,0 +1,40 @@
+
+function fooarg(this: any, a: any, b: any) {
+    this.a = a;
+    this.b =b
+    this.get = (target: object, prop: PropertyKey, receiver?: unknown) => {
+        console.log(target);
+        // TS 里面强制类型转换
+        return Reflect.get(...arguments as unknown as [target: object, prop: PropertyKey, receiver?: unknown]);
+    };
+}
+
+const a = {
+    get(){
+        
+    }
+}
+Reflect.apply(fooarg, a, ['222', '333']);
+
+console.log(a.a)
+console.log(a.b)
+fooarg('222', '333'); 
+
+// const target = {
+//     message1: "hello",
+//     message2: "everyone",
+//   };
+  
+//   const handler3 = {
+//     get(target: object, prop: PropertyKey, receiver?: unknown) {
+//       if (prop === "message2") {
+//         return "world";
+//       }
+//       return Reflect.get(...arguments as unknown as [target: object, prop: PropertyKey, receiver?: unknown]);
+//     },
+//   };
+  
+//   const proxy3 = new Proxy(target, handler3);
+  
+//   console.log(proxy3.message1); // hello
+//   console.log(proxy3.message2); // world
