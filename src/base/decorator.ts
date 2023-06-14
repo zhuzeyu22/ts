862d4e3785d74962102e023caf8ab39ff4df1565
zhuzeyu <zhuzeyu00@etest.cn>
2023-06-14 14:28:15 +0800

    update: decorator

--- src/base/decorator.ts
+++ src/base/decorator.ts
@@ -5,20 +5,52 @@
  */
 export function FunctionDecorator(id: string | symbol): MethodDecorator {
     return function <T>(target: Object, propertyKey: string | symbol, descriptor: TypedPropertyDescriptor<T>): TypedPropertyDescriptor<T> | void {
-        console.log(id);
-        console.log(target, propertyKey, descriptor);
-        console.log('qwe')
+        // console.log('FunctionDecorator', id);
+        // console.log('target', target);
+        // console.log('propertyKey', propertyKey);
+        console.log('descriptor', descriptor);
+        // console.log('FunctionDecorator qwe');
+        Reflect.deleteProperty(target, propertyKey);
+        console.log('target ', target);
+        // target[propertyKey] = () => { };
+        const ret = Reflect.set(target, propertyKey, () => { });
+        // console.log('reflect ', ret);
     };
 }
+
+
+export function FunctionDecorator2(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+
+    descriptor.value = function (...args: any[]) {
+        console.log(`Executing method: ${propertyKey}`);
+        //   return originalMethod.apply(this, args);
+        return null
+    };
+
+    return descriptor;
+};
 
 
 export class MyClass {
 
     @FunctionDecorator('fooId')
     foo(a: any) {
-        console.log('foo', a);
+        console.log('MyClass foo', a);
+    }
+
+    @FunctionDecorator2
+    foo2() {
+        console.log('foo2');
     }
 
 }
 
 
+const mc = new MyClass();
+
+// mc.foo('mc qwe');
+// console.log(mc);
+// console.log(mc.foo); 
+
+mc.foo2();
