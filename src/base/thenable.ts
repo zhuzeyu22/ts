39e2e33c36b1b85a9c39f24e418017092daced4f
zhuzeyu <zhuzeyu00@etest.cn>
2023-06-25 17:44:31 +0800

    update: base

--- src/base/thenable.ts
+++ src/base/thenable.ts
@@ -2,7 +2,7 @@
 const promise1 = Promise.resolve(123);
 
 promise1.then((value) => {
-    console.log(value);
+    console.log('promise1', value);
     // Expected output: 123
 });
 
@@ -10,15 +10,22 @@ promise1.then((value) => {
 export function something<T = unknown>(_do: string, ..._some: any[]): Thenable<T> {
     return {
         then() {
-            console.log(_do, ..._some);
+            console.log('_do, ..._some', _do, ..._some);
             console.log('this', this);
             return this;
         }
     };
 }
 
-const promise2 = Promise.resolve(something('I', "eat."));
-promise2.then((value) => {
-    console.log(value);
+const sth = something('I', "eat.")
+
+const promise2 = Promise.resolve(sth);
+
+
+console.log('end', promise2.then((value) => {
+    console.log('promise2', value);
+    console.log('end');
     // Expected output: 123
-});
+}));
+
+console.log('end2');
