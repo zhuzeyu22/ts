003268220f403342ca4a7528cae668a0662df88b
zhuzeyu22 <12524188+zhuzeyu22@user.noreply.gitee.com>
2023-07-17 15:08:28 +0800

    save

---
+++ src/base/glob.ts
@@ -0,0 +1,12 @@
+import { glob, globSync, globStream, globStreamSync, Glob } from 'glob';
+
+export const demo = () => {
+
+    const x = (<string[]>glob.sync('./*.ts'));
+
+    console.log(x);
+    debugger;
+    
+};
+
+demo()
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 // import { hello } from '@ace/hello';
-
 // console.log(hello())
+
+import { demo } from './base/glob';
+
+demo();
